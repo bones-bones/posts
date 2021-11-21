afd846975e697a32c9bc5d251f985007eafb45f4
bones-bones <elliot.ma.brown@gmail.com>
2021-11-21 15:40:09 -0800

    add a .env file

--- src/generateContent.ts
+++ src/generateContent.ts
@@ -4,22 +4,29 @@ import { formatPostContent } from './formatPost';
 import { headerFormatting } from './styles';
 import { POST_OUTPUT_PATH, OUTPUT_PATH } from './constants';
 import { generateSiteMap } from './siteMap';
-
+import { config } from 'dotenv';
+config();
 const textPosts = fs.readdirSync('textPosts').sort((a, b) => (a > b ? -1 : 1));
 
 fs.mkdirSync(POST_OUTPUT_PATH, { recursive: true });
 
-console.log(textPosts);
+console.log(textPosts, process.env.ROOT_URL);
 const entries = [];
-const headline = `<h1><a href="https://skeleton.club/posts" style="color:black;">Postings</a></h1>`;
+const headline = `<h1><a href="${process.env.ROOT_URL}/posts" style="color:black;">Posts</a></h1>${process}`;
 
 for (const textPost of textPosts) {
     const fileContent = fs.readFileSync('textPosts/' + textPost, 'utf8');
     const splitContent = fileContent.split('\n');
     const title = splitContent.shift()!;
     const date = splitContent.shift()!;
-    const themes = splitContent.shift()!.split(', ').filter(entry => entry !== '');
-    const threads = splitContent.shift()!.split(', ').filter(entry => entry !== '');
+    const themes = splitContent
+        .shift()!
+        .split(', ')
+        .filter((entry) => entry !== '');
+    const threads = splitContent
+        .shift()!
+        .split(', ')
+        .filter((entry) => entry !== '');
     const content = splitContent.join('\n');
     const postEntryData = formatPostEntry({
         title,
@@ -39,7 +46,7 @@ for (const textPost of textPosts) {
         date,
     });
 
-    console.log(threads)
+    console.log(threads);
     fs.writeFileSync(
         POST_OUTPUT_PATH + '/' + date + '.html',
         `
@@ -48,10 +55,15 @@ for (const textPost of textPosts) {
             `
         <meta property="og:title" content="${title}" />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="${threads.length > 0 ? 'threads: ' + threads.join(', ') + '.' : ''
-            }${themes.length > 0 ? 'themes: ' + themes.join(', ') : ''}" />
-        <meta property="og:image" content="https://skeleton.club/logo192.png" />
-        <link rel="icon" type="image/png" href="https://skeleton.club/Favicon.ico"></link>
+        <meta property="og:description" content="${
+            threads.length > 0 ? 'threads: ' + threads.join(', ') + '.' : ''
+        }${themes.length > 0 ? 'themes: ' + themes.join(', ') : ''}" />
+        <meta property="og:image" content="${
+            process.env.ROOT_URL
+        }/logo192.png" />
+        <link rel="icon" type="image/png" href="${
+            process.env.ROOT_URL
+        }/Favicon.ico"></link>
         `,
             title
         )}
@@ -72,13 +84,13 @@ fs.writeFileSync(
     `
     <html>
         ${headerFormatting(
-        `<link rel="icon" type="image/png" href="https://skeleton.club/Favicon.ico" />
-            <meta property="og:title" content="Skeleton Club - Posts" />
+            `<link rel="icon" type="image/png" href="${process.env.ROOT_URL}/Favicon.ico" />
+            <meta property="og:title" content="${process.env.SITE_TITLE}" />
             <meta property="og:type" content="website" />
             <meta property="og:description" content="A blog????." />
-            <meta property="og:image" content="https://skeleton.club/logo192.png" />`,
-        'Posts'
-    )}
+            <meta property="og:image" content="${process.env.ROOT_URL}/logo192.png" />`,
+            'Posts'
+        )}
         <body>
             ${headline}
             <div style="display: flex; justify-content: center;">
@@ -93,7 +105,7 @@ fs.writeFileSync(
 fs.writeFileSync(
     OUTPUT_PATH + '/' + 'siteMap.xml',
     generateSiteMap(
-        'https://skeleton.club/posts/post',
+        `${process.env.ROOT_URL}/posts/post`,
         textPosts.map((entry) => entry.split(' ')[0] + '.html')
     )
 );
